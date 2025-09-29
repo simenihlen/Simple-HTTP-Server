6788a923774f8c76f37f16fc4b221a04188d4d0a
Simen Ihlen <49519272+simenihlen@users.noreply.github.com>
2025-09-29 19:11:50 +0200

    From Layout

--- Simple HTTP Server/Form1.Designer.cs
+++ Simple HTTP Server/Form1.Designer.cs
@@ -23,13 +23,99 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.startServerBtn = new System.Windows.Forms.Button();
+            this.stopServerBtn = new System.Windows.Forms.Button();
+            this.serverPortText = new System.Windows.Forms.TextBox();
+            this.serverLogsText = new System.Windows.Forms.TextBox();
+            this.serverLogsLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startServerBtn
+            // 
+            this.startServerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startServerBtn.Location = new System.Drawing.Point(15, 47);
+            this.startServerBtn.Name = "startServerBtn";
+            this.startServerBtn.Size = new System.Drawing.Size(119, 23);
+            this.startServerBtn.TabIndex = 0;
+            this.startServerBtn.Text = "Start Server";
+            this.startServerBtn.UseVisualStyleBackColor = true;
+            // 
+            // stopServerBtn
+            // 
+            this.stopServerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopServerBtn.Location = new System.Drawing.Point(15, 92);
+            this.stopServerBtn.Name = "stopServerBtn";
+            this.stopServerBtn.Size = new System.Drawing.Size(119, 23);
+            this.stopServerBtn.TabIndex = 1;
+            this.stopServerBtn.Text = "Stop Server";
+            this.stopServerBtn.UseVisualStyleBackColor = true;
+            // 
+            // serverPortText
+            // 
+            this.serverPortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverPortText.Location = new System.Drawing.Point(15, 199);
+            this.serverPortText.Name = "serverPortText";
+            this.serverPortText.Size = new System.Drawing.Size(119, 22);
+            this.serverPortText.TabIndex = 2;
+            // 
+            // serverLogsText
+            // 
+            this.serverLogsText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverLogsText.Location = new System.Drawing.Point(247, 25);
+            this.serverLogsText.Multiline = true;
+            this.serverLogsText.Name = "serverLogsText";
+            this.serverLogsText.ReadOnly = true;
+            this.serverLogsText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.serverLogsText.Size = new System.Drawing.Size(541, 413);
+            this.serverLogsText.TabIndex = 3;
+            // 
+            // serverLogsLabel
+            // 
+            this.serverLogsLabel.AutoSize = true;
+            this.serverLogsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverLogsLabel.Location = new System.Drawing.Point(244, 6);
+            this.serverLogsLabel.Name = "serverLogsLabel";
+            this.serverLogsLabel.Size = new System.Drawing.Size(80, 16);
+            this.serverLogsLabel.TabIndex = 4;
+            this.serverLogsLabel.Text = "Server Logs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Server Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serverLogsLabel);
+            this.Controls.Add(this.serverLogsText);
+            this.Controls.Add(this.serverPortText);
+            this.Controls.Add(this.stopServerBtn);
+            this.Controls.Add(this.startServerBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startServerBtn;
+        private System.Windows.Forms.Button stopServerBtn;
+        private System.Windows.Forms.TextBox serverPortText;
+        private System.Windows.Forms.TextBox serverLogsText;
+        private System.Windows.Forms.Label serverLogsLabel;
+        private System.Windows.Forms.Label label2;
     }
 }
 
